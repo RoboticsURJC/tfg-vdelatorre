f072f3b14e4daf20939cb333d179045593b78fd1
vdelatorre2019 <v.delatorre.2019@alumnos.urjc.es>
2024-06-29 15:12:27 +0200

    imgs

--- global_navigation/image2binary.py
+++ global_navigation/image2binary.py
@@ -4,7 +4,6 @@ def image_to_binary_text(image_path, output_file):
     # Abrir la imagen en blanco y negro
     img = Image.open(image_path).convert('1')  # '1' convierte la imagen a blanco y negro
 
-    # Obtener tamaño de la imagen
     width, height = img.size
 
     # Crear un archivo de texto para escribir la representación binaria
@@ -12,21 +11,18 @@ def image_to_binary_text(image_path, output_file):
         # Iterar sobre cada pixel de la imagen
         for y in range(height):
             for x in range(width):
-                # Obtener el valor del pixel (0 o 255)
                 pixel = img.getpixel((x, y))
 
-                # Convertir el valor a binario (0 o 1)
                 bit_value = '0' if pixel == 0 else '1'
 
-                # Escribir el valor binario en el archivo de texto
                 f.write(bit_value)
 
-            f.write('\n')  # Nueva línea al final de cada fila
+            f.write('\n') 
 
 # Ejemplo de uso
 if __name__ == "__main__":
-    image_path = 'colors.png'  # Cambia esto a la ruta de tu imagen XCF
-    output_file = 'imagen_binaria.txt'  # Nombre del archivo de salida
+    image_path = 'colors.png'
+    output_file = 'imagen_binaria.txt'
 
     image_to_binary_text(image_path, output_file)
     print(f"Imagen convertida a binario y guardada en '{output_file}'.")
